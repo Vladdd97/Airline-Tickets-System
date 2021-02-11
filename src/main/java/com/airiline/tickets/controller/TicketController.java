174e0193a4597d8822c0f8d82ab70d73f7c347cb
Vlad <vladbantus02@gmail.com>
2021-02-11 22:36:17 +0200

    #2: added deleteTicketById endpoint

--- src/main/java/com/airiline/tickets/controller/TicketController.java
+++ src/main/java/com/airiline/tickets/controller/TicketController.java
@@ -8,13 +8,7 @@ import com.airiline.tickets.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -36,5 +30,11 @@ public class TicketController {
     @PatchMapping("/{ticketId}")
     public ResponseEntity<TicketResponse> update(@RequestBody UpdateTicketRequest ticket, @PathVariable Long ticketId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ticketService.update(ticket, ticketId));
+    }
+
+    @DeleteMapping("/{ticketId}")
+    public ResponseEntity<Void> delete(@PathVariable Long ticketId) {
+        ticketService.deleteById(ticketId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
