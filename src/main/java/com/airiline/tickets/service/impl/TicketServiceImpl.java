2ee034219e7c425f00a80b9d2ff33af04559bae0
Vlad <vladbantus02@gmail.com>
2021-02-11 19:28:49 +0200

    ATS-11: added and configured TicketMapper

--- src/main/java/com/airiline/tickets/service/impl/TicketServiceImpl.java
+++ src/main/java/com/airiline/tickets/service/impl/TicketServiceImpl.java
@@ -1,8 +1,8 @@
 package com.airiline.tickets.service.impl;
 
-import com.airiline.tickets.domain.Ticket;
 import com.airiline.tickets.dto.CreateTicketRequest;
 import com.airiline.tickets.dto.CreateTicketResponse;
+import com.airiline.tickets.mapper.TicketMapper;
 import com.airiline.tickets.repository.TicketRepository;
 import com.airiline.tickets.service.TicketService;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +16,7 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public CreateTicketResponse save(CreateTicketRequest ticketRequest) {
-        var ticket = ticketRepository.save(createTicket(ticketRequest));
-        return createTicketResponse(ticket);
+        var ticket = TicketMapper.INSTANCE.createTicketRequestToTicket(ticketRequest);
+        return TicketMapper.INSTANCE.ticketToCreateTicketResponse(ticketRepository.save(ticket));
     }
-
-    private Ticket createTicket(CreateTicketRequest ticketRequest) {
-        return Ticket.builder()
-                .departureLocation(ticketRequest.getDepartureLocation())
-                .destinationLocation(ticketRequest.getDestinationLocation())
-                .flightPrice(ticketRequest.getFlightPrice())
-                .build();
-    }
-
-    private CreateTicketResponse createTicketResponse(Ticket ticket) {
-        return CreateTicketResponse.builder()
-                .id(ticket.getId())
-                .departureLocation(ticket.getDepartureLocation())
-                .destinationLocation(ticket.getDestinationLocation())
-                .flightPrice(ticket.getFlightPrice())
-                .build();
-    }
-
 }
