e871b077fe7c9af65655d25fac74abd7ba74dbf9
Vlad <vladbantus02@gmail.com>
2021-03-05 10:31:30 +0200

    ATS-40: cached request to open weather map by city name

--- src/main/java/com/airiline/tickets/client/openweathermap/dto/SearchWeatherDataResponse.java
+++ src/main/java/com/airiline/tickets/client/openweathermap/dto/SearchWeatherDataResponse.java
@@ -2,11 +2,13 @@ package com.airiline.tickets.client.openweathermap.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class SearchWeatherDataResponse {
     private String message;
     private int cod;
--- src/main/java/com/airiline/tickets/service/impl/WeatherServiceImpl.java
+++ src/main/java/com/airiline/tickets/service/impl/WeatherServiceImpl.java
@@ -5,7 +5,10 @@ import com.airiline.tickets.client.openweathermap.OpenWeatherMapServiceClient;
 import com.airiline.tickets.client.openweathermap.dto.SearchWeatherDataResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import static com.airiline.tickets.configuration.CachingConfiguration.WEATHER_CACHE;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class WeatherServiceImpl implements WeatherService {
     private final OpenWeatherMapServiceClient weatherMapServiceClient;
 
     @Override
+    @Cacheable(value = WEATHER_CACHE, key = "#cityName")
     public SearchWeatherDataResponse getWeatherByCityName(String cityName) {
         return weatherMapServiceClient.searchWeatherData(cityName);
     }
